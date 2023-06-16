56697a3594553a56724e88a35d1c6d4ce803382a
Megidd Git <megiddgit@gmail.com>
2023-06-16 05:45:48 -0700

    Stackoverflow question

--- structural/newUI.lua
+++ structural/newUI.lua
@@ -1,36 +1,32 @@
 platform.apilevel = '2.2'
 
-desc1, error = D2Editor.newRichText():resize(200, 40)
-desc1:move(0, 0):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(false)
+box_prompt, error = D2Editor.newRichText():resize(300, 40)
+box_prompt:move(0, 0):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(false)
     :setTextColor(0x666666):setVisible(true)
-result, error = desc1:setText('Ant [net tension area]')
+result, error = box_prompt:setText('Please enter an expression in box below')
 
-ed1, error = D2Editor.newRichText():resize(200, 40)
-ed1:move(0, 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
+box_expr, error = D2Editor.newRichText():resize(300, 40)
+box_expr:move(0, 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
-result, error = ed1:setText('0.0')
-str, pos, sel, error = ed1:getExpressionSelection()
--- ed1:createMathBox()
 
-ed2, error = D2Editor.newRichText():resize(200, 40)
-ed2:move(0, 100):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
+box_result, error = D2Editor.newRichText():resize(300, 40)
+box_result:move(0, 100):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
--- d2e2:createMathBox()
 
-ed3, error = D2Editor.newRichText():resize(200, 40)
-ed3:move(0, 150):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(false)
-    :setTextColor(0x666666):setVisible(true)
+box_expr_doublecheck, error = D2Editor.newRichText():resize(300, 40)
+box_expr_doublecheck:move(0, 150):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(
+    false):setTextColor(0x666666):setVisible(true)
 
 -- To evaluate the expression
 function run()
-    local expression = ed1:getExpression()
-    ed3:setText(expression)
+    local expression = box_expr:getExpression()
+    box_expr_doublecheck:setText("expression doublecheck: "..expression)
     local result, err = math.evalStr(expression)
     if err ~= nil then
         show_error(err)
         return
     end
-    ed2:setText("Result: " .. result)
+    box_result:setText("Result: " .. result)
 end
 
 function on.escapeKey()
@@ -40,6 +36,6 @@ end
 function show_error(err)
     if err == nil then
     else
-        ed2:setText("Error: " .. err)
+        box_result:setText("Error: " .. err)
     end
 end
