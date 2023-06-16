f28a3991df6e28795a305c28a39a5eb99fbfafcf
Megidd Git <megiddgit@gmail.com>
2023-06-16 11:06:19 -0700

    Delete unnecessary math box

--- structural/newUI.lua
+++ structural/newUI.lua
@@ -8,22 +8,21 @@ result, error = eP:setText('Ant [net tension area]')
 eE, error = D2Editor.newRichText():resize(300, 40)
 eE:move(0, 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
-mbE = eE:createMathBox() -- Math Box
 
 eR, error = D2Editor.newRichText():resize(300, 40)
 eR:move(0, 100):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
 
 eD, error = D2Editor.newRichText():resize(300, 40)
-eD:move(0, 150):setBorder(1):setBorderColor(0x43adee):setFontSize(6):setReadOnly(true):setSelectable(true)
-    :setTextColor(0x666666):setVisible(true)
+eD:move(0, 150):setBorder(1):setBorderColor(0x43adee):setFontSize(6):setReadOnly(true):setSelectable(true):setTextColor(
+    0x666666):setVisible(true)
 
 -- To evaluate the expression
 function run()
-    local mu = mbE:getExpression()
+    local general = eE:getExpression()
     -- Math Box markup is "\0el {...}"
-    local E = delete_markup(mu)
-    eD:setText("markup:" .. mu .. "   string:" .. E)
+    local E = delete_markup(general)
+    eD:setText("markup:" .. general .. "   string:" .. E)
     local result, err = math.evalStr(E)
     if err ~= nil then
         show_error(err)
