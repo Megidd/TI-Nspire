47ff6626eda2ea7486d8644dc5d96245dc47cc08
Megidd Git <megiddgit@gmail.com>
2023-06-16 09:58:04 -0700

    Increase width

--- structural/newUI.lua
+++ structural/newUI.lua
@@ -1,20 +1,20 @@
 platform.apilevel = '2.2'
 
-eP, error = D2Editor.newRichText():resize(200, 40)
+eP, error = D2Editor.newRichText():resize(300, 40)
 eP:move(0, 0):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(false):setTextColor(
     0x666666):setVisible(true)
 result, error = eP:setText('Ant [net tension area]')
 
-eE, error = D2Editor.newRichText():resize(200, 40)
+eE, error = D2Editor.newRichText():resize(300, 40)
 eE:move(0, 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
 mbE = eE:createMathBox() -- Math Box
 
-eR, error = D2Editor.newRichText():resize(200, 40)
+eR, error = D2Editor.newRichText():resize(300, 40)
 eR:move(0, 100):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false):setSelectable(true)
     :setTextColor(0x000000):setVisible(true)
 
-eD, error = D2Editor.newRichText():resize(200, 40)
+eD, error = D2Editor.newRichText():resize(300, 40)
 eD:move(0, 150):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true):setSelectable(true)
     :setTextColor(0x666666):setVisible(true)
 
