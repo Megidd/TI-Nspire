9c06c588b57c6147ab9c8643a4add4553fb40cff
Megidd Git <megiddgit@gmail.com>
2023-06-30 16:27:47 +0330

    Fix: draw editors once and update their position:
    The `on.paint(gc)` function is called every time the screen needs to be redrawn, such as when scrolling or resizing. As a result, you're creating a new set of rich text editors each time the function is called, which is causing the crowding on the screen.
    
    To avoid this, you should create the rich text editors outside of the `on.paint(gc)` function and only update their positions and visibility inside the function.

--- structural/RectangularHSS.lua
+++ structural/RectangularHSS.lua
@@ -45,25 +45,37 @@ local scroll_offset = 0 -- Add a variable to track the vertical scroll offset
 local scroll_offset_x = 0
 local help = false
 
+local editors = {}
+
+-- Create the rich text editors outside of the on.paint(gc) function and
+-- only update their positions.
+for i = 1, #numbers do
+    local y = scroll_offset -- Subtract the scroll offset from the y-coordinate of the text
+    local x = scroll_offset_x
+    local eP, error = D2Editor.newRichText():resize(300, 40)
+    eP:move(x + 8, y + (2 * i + 0) * 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true)
+        :setSelectable(false):setTextColor(0x666666):setVisible(true)
+    eP:setText(prompts[i] .. "  " .. descriptions[i])
+
+    local eI, error = D2Editor.newRichText():resize(300, 40)
+    eI:move(x + 8, y + (2 * i + 1) * 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false)
+        :setSelectable(true):setTextColor(0x000000):setVisible(true)
+    eI:setText(prompts[i] .. "  " .. descriptions[i])
+
+    editors[2 * i + 0] = eP
+    editors[2 * i + 1] = eI
+end
+
 function on.paint(gc)
     local y = scroll_offset -- Subtract the scroll offset from the y-coordinate of the text
     local x = scroll_offset_x
 
-    local inputs = {}
-
     for i = 1, #numbers do
-        local eP, error = D2Editor.newRichText():resize(300, 40)
-        eP:move(x + 8, y + (2 * i + 0) * 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(true)
-            :setSelectable(false):setTextColor(0x666666):setVisible(true)
-        eP:setText(prompts[i] .. "  " .. descriptions[i])
+        local eP = editors[2 * i + 0]
+        local eI = editors[2 * i + 1]
 
-        local eI, error = D2Editor.newRichText():resize(300, 40)
-        eI:move(x + 8, y + (2 * i + 1) * 50):setBorder(1):setBorderColor(0x43adee):setFontSize(12):setReadOnly(false)
-            :setSelectable(true):setTextColor(0x000000):setVisible(true)
-        eI:setText(prompts[i] .. "  " .. descriptions[i])
-
-        inputs[2 * i + 0] = eP
-        inputs[2 * i + 1] = eI
+        eP:move(x + 8, y + (2 * i + 0) * 50)
+        eI:move(x + 8, y + (2 * i + 1) * 50)
     end
 
     -- Just show help and return.
